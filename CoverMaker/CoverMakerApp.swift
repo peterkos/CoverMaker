1556b64aa2ab7a5980380408e4930d39842b7e22
Peter Kos <pkos91@icloud.com>
2024-05-24 23:45:44 -0400

    Initial Commit

---
+++ CoverMaker/CoverMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverMakerApp.swift
+//  CoverMaker
+//
+//  Created by Peter Kos on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CoverMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
